09f7c36d9a4cf43575876d46c6bf5332b0e882a9
vad7 <vad7@yahoo.com>
2021-04-30 13:19:19 +0300

    upd

--- ControlHeatPump/Control/VaconFC.h
+++ ControlHeatPump/Control/VaconFC.h
@@ -1,6 +1,5 @@
 /*
  * Copyright (c) 2016-2020 by Vadim Kulakov vad7@yahoo.com, vad711
- * &                       by Pavel Panfilov <firstlast2007@gmail.com> skype pav2000pav
  * Частотный преобразователь Vacon 10
  * Автор vad711, vad7@yahoo.com
  *
