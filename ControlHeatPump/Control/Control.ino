1c8a33680c0a935cf2e3e34bc1b394aa6e0a0d7a
vad7 <vad7@yahoo.com>
2024-08-10 12:15:38 +0300

    upd

--- ControlHeatPump/Control/Control.ino
+++ ControlHeatPump/Control/Control.ino
@@ -1429,7 +1429,7 @@ xNOPWR_OtherLoad:					uint32_t t = rtcSAM3X8.unixtime();
 #ifdef WEATHER_FORECAST
 			if(active && rtcSAM3X8.get_days() != WF_Day) {
 				WF_BoilerTargetPercent = 100;
-				if(rtcSAM3X8.get_hours() * 100 + rtcSAM3X8.get_minutes() >= WR.WF_Time * 10 && strlen(HP.Option.WF_ReqServer)) {
+				if(rtcSAM3X8.get_hours() == WR.WF_Time / 10 && rtcSAM3X8.get_minutes() >= WR.WF_Time % 10 && strlen(HP.Option.WF_ReqServer)) {
 					if(!active) WEB_SERVER_MAIN_TASK();	/////////////////////////////////////// Выполнить задачу веб сервера
 					int err = Send_HTTP_Request(HP.Option.WF_ReqServer, NULL, HP.Option.WF_ReqText, 4);
 					if(err == OK) err = WF_ProcessForecast(Socket[MAIN_WEB_TASK].outBuf);
